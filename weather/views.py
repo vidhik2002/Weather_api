98884d4b2f658af21a9f93964610ef93c0c73cde
Vidhi Khaitan <vidhik2002@gmail.com>
2021-04-29 11:02:08 +0530

    feat: update form

--- weather/views.py
+++ weather/views.py
@@ -5,12 +5,32 @@ from .forms import MainForm
 
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=API_KEY'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=b0f11ba775d36e6d23cc493a249875af'
+    error = ""
+    msg = ""
+    msg_class = ""
 
     if request.method == 'POST':
         form = MainForm(request.POST)
-        form.save()
 
+        if form.is_valid():
+            fresh_city = form.cleaned_data['name']
+            city_count = City.objects.filter(name=fresh_city).count()
+            if city_count == 0:
+                req = requests.get(url.format(fresh_city)).json()
+                if req['cod'] == 200:
+                    form.save()
+                else:
+                    error = "City doesnot exist "
+            else:
+                error = "City already there"
+
+        if error:
+            msg = error
+            msg_class = 'is-danger'
+        else:
+            msg = "City added successfully"
+            msg_class = 'is-success'
     form = MainForm()
     cities = City.objects.all()
 
@@ -27,5 +47,9 @@ def index(request):
             'icon': r['weather'][0]['icon']
         }
         all_weather_data.append(city_weather_conditions)
-    context = {'weather_data': all_weather_data, 'form': form}
+    context = {'weather_data': all_weather_data,
+               'form': form,
+               'msg': msg,
+               'msg_class': msg_class
+            }
     return render(request, 'home.html', context)
